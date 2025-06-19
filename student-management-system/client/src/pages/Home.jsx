6297481922a7bb8a03805dfe1c4646ebe4c54fa9
males <malesh26032006@gmail.com>
2025-06-19 16:17:18 +0530

    Add new folders and update some files

--- student-management-system/client/src/pages/Home.jsx
+++ student-management-system/client/src/pages/Home.jsx
@@ -4,17 +4,17 @@ import { useState, useEffect } from 'react';
 function Home() {
     const [students, setStudents] = useState([]);
     useEffect(() => {
-        fetch("http://localhost:5000/api/students")
-        .then((res) => res.json())
-        .then((data) => setStudents(data))
-        .catch((error) => {
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/api/students`)
+          .then((res) => res.json())
+          .then((data) => setStudents(data))
+          .catch((error) => {
             console.error("Error fetching students:", error);
             alert("Failed to fetch students. Please try again.");
-        });
+          });
     }, []);
 
     const handleDelete = async (id) => {
-        await fetch(`http://localhost:5000/api/students/${id}`, {
+        await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/students/${id}`, {
             method: 'DELETE',
         });
         setStudents((prev) => prev.filter((s) => s._id !== id));
@@ -23,7 +23,7 @@ function Home() {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             { students.length === 0 ? (
-                <p>No students added yet.</p>
+                <p className="bg-gray-200 text-gray-700 p-4 rounded-lg shadow-md text-center">No students added yet.</p>
             ) : (
                 students.map((s) => (
                     <StudentCard key={s._id} student={s} onDelete={handleDelete} />
